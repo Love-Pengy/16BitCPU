08e408665014730b84631ee1ba91921182c167b1
Love-Pengy <afrazierboy2001@gmail.com>
2024-10-30 09:56:51 -0400

    [FEAT] add new functions to ALU

---
+++ 16BitCPU/16BitCPU.srcs/sources_1/new/aluDesign.vhd
@@ -0,0 +1,221 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: Brandon Frazier
+-- 
+-- Create Date: 09/17/2024 03:05:11 PM
+-- Design Name: 
+-- Module Name: aluDesign - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+use ieee.numeric_std.ALL;
+entity ALU is
+
+    generic(N : integer := 16);
+    port(
+         -- in 1
+         A: in std_logic_vector(N-1 downto 0);
+         -- in 2
+         B: in std_logic_vector(N-1 downto 0);
+         -- result
+         C: out std_logic_vector(N-1 downto 0);
+         -- 0000 Addition 
+         -- 0001 Subtraction
+         -- 0010 Bitwise AND
+         -- 0011 Bitwise OR
+         -- 0100 sgt 
+         -- 0101 slt
+         -- 0110 XOR
+         -- 0111 NAND
+         -- 1000 XNOR
+         -- 1001 CUTL
+         -- 1010 CUTU
+         -- 1011 SLL
+         -- 1100 SRL
+
+        Mode: in std_logic_vector(3 downto 0);
+        -- Output Flags
+        Zero: out std_logic
+        --Overflow: out std_logic;
+        --Cout: out std_logic
+        );
+        
+end ALU;
+
+
+architecture Behavioral of ALU is
+
+    signal addOutput, twoOutput, shiftOutput, subOutput, bitOutput, compOutput, shiftIndex: std_logic_vector(N-1 downto 0);
+    signal comparatorOutput: std_logic_vector(1 downto 0);
+    COMPONENT nBitAdder
+        PORT (
+            A, B: in std_logic_vector(N-1 downto 0);
+            Cin: in std_logic;
+            Cout, Overflow: out std_logic;
+            C: out std_logic_vector(N-1 downto 0));
+    END COMPONENT;
+    
+    FOR ALL : nBitAdder USE ENTITY work.nBitAdder(Behavioral);
+
+    COMPONENT comparator 
+        PORT (
+            in1, in2: in std_logic_vector(N-1 downto 0);
+            output: out std_logic_vector(1 downto 0));
+    END COMPONENT;
+    
+    FOR ALL : comparator USE ENTITY work.comparator(Behavioral);
+    
+    begin
+    
+    
+    adder: nBitAdder
+      PORT MAP(A => A, B => B, Cin => '0', C => addOutput, Overflow => open, Cout => open);
+    
+    subtractor: nBitAdder
+      PORT MAP(A => A, B => twoOutput, Cin => '0', C => subOutput, Overflow => open, Cout => open);
+      
+    -- open (https://stackoverflow.com/questions/19412165/how-to-ignore-output-ports-with-port-maps)
+    twoCompliment: nBitAdder
+      PORT MAP(A => X"0001", B => (NOT B), Cin => '0', C => twoOutput, Overflow => open, Cout => open);
+      
+    compare: comparator
+      PORT MAP(in1 => A, in2 => B, output => comparatorOutput); 
+      
+    process(A, B, Mode,subOutput)
+        variable cutCalc : std_logic_vector(15 downto 0);
+        begin
+        case Mode is
+            -- addition
+            when "0000" =>
+              C <= addOutput;
+              if(addOutput = X"0000") then
+                Zero <= '1';
+              else 
+                Zero <= '0';
+              end if;  
+            -- subtraction      
+            when "0001" => 
+              C <= subOutput;
+              if(subOutput = X"0000") then
+                Zero <= '1';
+              else 
+                Zero <= '0';
+              end if;     
+            -- bitwise AND         
+            when "0010" => 
+                bitOutput <= A AND B;
+                if(bitOutput = X"0000") then 
+                    Zero <= '1';
+                else 
+                    Zero <= '0';
+                end if;      
+                C <= bitOutput;   
+            -- bitwise OR       
+            when "0011" =>
+                bitOutput <= A OR B;
+                if(bitOutput = X"0000") then 
+                    Zero <= '1';
+                else 
+                    Zero <= '0';
+                end if;    
+                C <= bitOutput;
+            -- sgt
+            when "0100" => 
+                compOutput <= X"0001" when (comparatorOutput = "10") else X"0000";  
+                zero <= '1' when (compOutput = X"0000") else '0';
+                C <= compOutput;
+            -- slt
+            when "0101" => 
+                 compOutput <= X"0001" when (comparatorOutput = "01") else X"0000";  
+                zero <= '1' when (compOutput = X"0000") else '0';
+                C <= compOutput;
+            -- XOR
+            when "0110" =>
+                bitOutput <= A XOR B;
+                if(bitOutput = X"0000") then 
+                    Zero <= '1';
+                else 
+                    Zero <= '0';
+                end if;
+                C <= bitOutput;
+            -- NAND
+            when "0111" => 
+                bitOutput <= A NAND B;
+                if(bitOutput <= X"0000") then 
+                    Zero <= '1';
+                 else 
+                    Zero <= '0';
+                 end if;
+                C <= bitOutput;
+            -- XNOR
+            when "1000" => 
+                bitOutput <= A XNOR B;
+                if(bitOutput = X"0000") then 
+                    Zero <= '1';
+                else 
+                    Zero <= '0';
+                end if;
+                C <= bitOutput;
+            -- CUTL
+            when "1001" => 
+                cutCalc := A;
+                for i in 0 to to_integer(unsigned(B)) LOOP
+                    cutCalc(i) := '0';
+                end LOOP;
+                if(cutCalc = X"0000") then  
+                    Zero <= '1';
+                else 
+                    Zero <= '0';
+                end if;
+                C <= cutCalc;
+            -- CUTU 
+            when "1010" => 
+                 cutCalc := A;
+                for i in 15 downto (15 - to_integer(unsigned(B)))  LOOP
+                    cutCalc(i) := '0';
+                end LOOP;
+                if(cutCalc = X"0000") then  
+                    Zero <= '1';
+                else 
+                    Zero <= '0';
+                end if;
+                C <= cutCalc;
+            -- SLL
+            when "1011" =>
+                bitOutput <= A SLL to_integer(unsigned(B));
+                if(bitOutput <= X"0000") then 
+                    Zero <= '1';
+                 else 
+                    Zero <= '0';
+                 end if;
+                C <= bitOutput;
+            -- SRL
+            when "1100" =>  
+                bitOutput <= A SRL to_integer(unsigned(B));
+                if(bitOutput <= X"0000") then 
+                    Zero <= '1';
+                 else 
+                    Zero <= '0';
+                 end if;
+                C <= bitOutput;  
+            when others =>
+                --nothing
+        end case;
+    
+    end process;
+    
+
+
+
+end Behavioral;
